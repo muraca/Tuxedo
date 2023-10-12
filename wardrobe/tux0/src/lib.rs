7b06a37f8fdafb8b8171225a3fd627f7c1804e9c
muraca <mmuraca247@gmail.com>
2023-10-12 12:26:37 +0200

    transfer verifies encryption split into mint and transfer
    Signed-off-by: muraca <mmuraca247@gmail.com>

--- wardrobe/tux0/src/lib.rs
+++ wardrobe/tux0/src/lib.rs
@@ -5,25 +5,22 @@
 use money::Coin;
 use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
-#[cfg(feature = "std")]
-use serde::{Deserialize, Serialize};
 use sp_core::H256;
 use sp_runtime::{transaction_validity::TransactionPriority, SaturatedConversion};
 use sp_std::prelude::*;
 use tuxedo_core::{
     dynamic_typing::{DynamicallyTypedData, UtxoData},
-    SimpleConstraintChecker,
+    ensure,
+    types::{Output, OutputRef, Transaction},
+    utxo_set::TransparentUtxoSet,
+    ConstraintChecker, SimpleConstraintChecker, Verifier,
 };
+
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
 
 #[cfg(test)]
 mod tests;
-
-/// The main constraint checker for the utx0 piece.
-/// The only operation supported is a transfer of Coins or DAPCoins of the same ID.
-/// The value of the consumed Coins must be greater or equal to the value of the created Coins.
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Hash, Debug, TypeInfo)]
-pub struct Utx0ConstraintChecker<const ID: u8>;
 
 /// A single coin in the DAP money system.
 /// A new-type wrapper around a hashed value.
@@ -50,12 +47,18 @@ pub enum ConstraintCheckerError {
     OutputsExceedInputs,
 }
 
+/// Computes the total value from a list of DynamicallyTypedData.
+/// If allow_money is true, then Coins are allowed in the list, otherwise it fails.
+/// If allow_dap is true, then DAPCoins are allowed in the list, otherwise it fails.
 fn total_value<const ID: u8>(
     data: &[DynamicallyTypedData],
+    allow_money: bool,
+    allow_dap: bool,
 ) -> Result<u128, ConstraintCheckerError> {
     let mut total: u128 = 0;
     for item in data {
         if item.type_id == Coin::<ID>::TYPE_ID {
+            ensure!(allow_money, ConstraintCheckerError::BadlyTyped);
             let utxo_value = item
                 .extract::<Coin<ID>>()
                 .map_err(|_| ConstraintCheckerError::BadlyTyped)?
@@ -64,6 +67,7 @@ fn total_value<const ID: u8>(
                 .checked_add(utxo_value)
                 .ok_or(ConstraintCheckerError::ValueOverflow)?;
         } else if item.type_id == DAPCoin::<ID>::TYPE_ID {
+            ensure!(allow_dap, ConstraintCheckerError::BadlyTyped);
             total = total
                 .checked_add(1)
                 .ok_or(ConstraintCheckerError::ValueOverflow)?;
@@ -74,7 +78,14 @@ fn total_value<const ID: u8>(
     Ok(total)
 }
 
-impl<const ID: u8> SimpleConstraintChecker for Utx0ConstraintChecker<ID> {
+/// The only operation supported by this ConstraintChecker is the mint,
+/// that consumes Coins to produce DAPCoins of the same ID.
+/// The number of the produced DAPCoins must be less or equal to the Value of the consumed Coins.
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Hash, Debug, TypeInfo)]
+pub struct Tux0Mint<const ID: u8>;
+
+impl<const ID: u8> SimpleConstraintChecker for Tux0Mint<ID> {
     type Error = ConstraintCheckerError;
 
     fn check(
@@ -83,9 +94,114 @@ impl<const ID: u8> SimpleConstraintChecker for Utx0ConstraintChecker<ID> {
         _peeks: &[DynamicallyTypedData],
         output_data: &[DynamicallyTypedData],
     ) -> Result<TransactionPriority, Self::Error> {
-        // Check that sum of input values < output values
-        let total_input_value = total_value::<ID>(input_data)?;
-        let total_output_value = total_value::<ID>(output_data)?;
+        // Only allow Coins as inputs.
+        let total_input_value = total_value::<ID>(&input_data, true, false)?;
+        // Only allow DAPCoins as outputs.
+        let total_output_value = total_value::<ID>(&output_data, false, true)?;
+
+        if total_input_value >= total_output_value {
+            Ok((total_input_value - total_output_value).saturated_into())
+        } else {
+            Err(ConstraintCheckerError::OutputsExceedInputs)
+        }
+    }
+}
+
+/// A mock random number generator that always returns 0.
+struct MockRng;
+impl rand::CryptoRng for MockRng {}
+impl rand::RngCore for MockRng {
+    fn next_u32(&mut self) -> u32 {
+        0
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        0
+    }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        for i in dest.iter_mut() {
+            *i = 0;
+        }
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand::Error> {
+        self.fill_bytes(dest);
+        Ok(())
+    }
+}
+
+#[derive(Debug, Encode, Decode, Clone, TypeInfo)]
+pub struct SpendData {
+    pub pubkey: H256,
+    pub secret: H256,
+    pub utxo_ref: OutputRef,
+}
+
+/// The Verifier used along Tux0Transfer, to check that a DAP Coin's secret is revealed correctly before spending it.
+#[derive(Debug, Encode, Decode, Clone, TypeInfo)]
+pub struct Tux0TransferVerifier<const ID: u8>;
+
+impl<const ID: u8> Verifier for Tux0TransferVerifier<ID> {
+    fn verify(&self, simplified_tx: &[u8], redeemer: &[u8]) -> bool {
+        // Check that the transaction is valid and uses the right ConstraintChecker.
+        let Ok(transaction) =
+            Transaction::<Self, Tux0Transfer<ID>>::decode(&mut &simplified_tx[..])
+        else {
+            return false;
+        };
+
+        let Ok(spend_data) = SpendData::decode(&mut &redeemer[..]) else {
+            return false;
+        };
+
+        if transaction
+            .inputs
+            .iter()
+            .find(|input| input.output_ref == spend_data.utxo_ref)
+            .is_none()
+        {
+            return false;
+        };
+
+        let Ok(pubkey) = ecies_ed25519::PublicKey::from_bytes(&spend_data.pubkey.0) else {
+            return false;
+        };
+
+        ecies_ed25519::encrypt(&pubkey, &spend_data.secret.0, &mut MockRng {}).unwrap_or_default()
+            == TransparentUtxoSet::<Self>::peek_utxo(&spend_data.utxo_ref)
+                .expect("existence of UTXO already verified by executive")
+                .payload
+                .data
+    }
+}
+
+/// The only operation supported by this checker is a transfer,
+/// which consumes DAPCoins only, and produces Coins or DAPCoins of the same ID.
+/// The value of the consumed Coins must be greater or equal to the value of the created Coins.
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Hash, Debug, TypeInfo)]
+pub struct Tux0Transfer<const ID: u8>;
+
+// This is a ConstraintChecker instead of a SimpleConstraintChecker to only allow the Tux0TransferVerifier.
+impl<const ID: u8> ConstraintChecker<Tux0TransferVerifier<ID>> for Tux0Transfer<ID> {
+    type Error = ConstraintCheckerError;
+
+    fn check(
+        &self,
+        inputs: &[Output<Tux0TransferVerifier<ID>>],
+        _peeks: &[Output<Tux0TransferVerifier<ID>>],
+        outputs: &[Output<Tux0TransferVerifier<ID>>],
+    ) -> Result<TransactionPriority, Self::Error> {
+        let input_data: Vec<DynamicallyTypedData> =
+            inputs.iter().map(|i| i.payload.clone()).collect();
+        let output_data: Vec<DynamicallyTypedData> =
+            outputs.iter().map(|o| o.payload.clone()).collect();
+
+        // Only allow DAPCoins as inputs.
+        let total_input_value = total_value::<ID>(&input_data, false, true)?;
+        // Allow both Coins and DAPCoins as outputs.
+        let total_output_value = total_value::<ID>(&output_data, true, true)?;
 
         if total_input_value >= total_output_value {
             Ok((total_input_value - total_output_value).saturated_into())
--- wardrobe/tux0/src/tests.rs
+++ wardrobe/tux0/src/tests.rs
@@ -5,23 +5,24 @@ use sp_runtime::traits::{BlakeTwo256, Hash};
 #[test]
 fn mint_valid_transaction_works() {
     assert_eq!(
-        Utx0ConstraintChecker::<0>.check(
-            &[Coin::<0>(5).into()],
+        SimpleConstraintChecker::check(
+            &Tux0Mint::<0>,
+            &[Coin::<0>(1).into(), Coin::<0>(4).into()],
             &[],
             &[
                 DAPCoin::<0>(BlakeTwo256::hash_of(&0u8)).into(),
                 DAPCoin::<0>(BlakeTwo256::hash_of(&1u8)).into(),
-                Coin::<0>(2).into()
             ]
         ),
-        Ok(1)
+        Ok(3)
     );
 }
 
 #[test]
 fn mint_no_input_fails() {
     assert_eq!(
-        Utx0ConstraintChecker::<0>.check(
+        SimpleConstraintChecker::check(
+            &Tux0Mint::<0>,
             &[],
             &[],
             &[DAPCoin::<0>(BlakeTwo256::hash_of(&0u8)).into()]
@@ -34,7 +35,7 @@ fn mint_no_input_fails() {
 fn mint_no_output_works() {
     // This should work, as it is a valid transaction, which burns all the input.
     assert_eq!(
-        Utx0ConstraintChecker::<0>.check(&[Coin::<0>(3).into()], &[], &[]),
+        SimpleConstraintChecker::check(&Tux0Mint::<0>, &[Coin::<0>(3).into()], &[], &[]),
         Ok(3)
     );
 }
@@ -42,8 +43,9 @@ fn mint_no_output_works() {
 #[test]
 fn mint_money_creation_fails() {
     assert_eq!(
-        Utx0ConstraintChecker::<0>.check(
-            &[DAPCoin::<0>(BlakeTwo256::hash_of(&0u8)).into()],
+        SimpleConstraintChecker::check(
+            &Tux0Mint::<0>,
+            &[Coin::<0>(1).into()],
             &[],
             &[
                 DAPCoin::<0>(BlakeTwo256::hash_of(&1u8)).into(),
@@ -53,3 +55,34 @@ fn mint_money_creation_fails() {
         Err(ConstraintCheckerError::OutputsExceedInputs)
     );
 }
+
+#[test]
+fn mint_invalid_input() {
+    assert_eq!(
+        SimpleConstraintChecker::check(
+            &Tux0Mint::<0>,
+            &[DAPCoin::<0>(BlakeTwo256::hash_of(&0u8)).into()],
+            &[],
+            &[DAPCoin::<0>(BlakeTwo256::hash_of(&2u8)).into(),]
+        ),
+        Err(ConstraintCheckerError::BadlyTyped)
+    )
+}
+
+#[test]
+fn mint_invalid_output() {
+    assert_eq!(
+        SimpleConstraintChecker::check(
+            &Tux0Mint::<0>,
+            &[Coin::<0>(5).into()],
+            &[],
+            &[
+                DAPCoin::<0>(BlakeTwo256::hash_of(&1u8)).into(),
+                Coin::<0>(4).into(),
+            ]
+        ),
+        Err(ConstraintCheckerError::BadlyTyped)
+    )
+}
+
+// TODO: implement transfer tests
